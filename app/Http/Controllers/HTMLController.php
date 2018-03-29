6de7a774d578c5aa7bc2d1567cfee229fec65625
sergiomakaruk <sergiomakaruk@gmail.com>
2018-03-29 16:55:04 -0300

    daTOLA

--- app/Http/Controllers/HTMLController.php
+++ app/Http/Controllers/HTMLController.php
@@ -7,39 +7,30 @@ use Illuminate\Support\Facades\File;
 
 class HTMLController extends Controller
 {
+
+  /*
+  FTP:
+Usuario: expopyme
+Contraseña: 3zzyoY8W10
+URL del Panel de Control: https://www7.baehost.com:2083/
+ */
+
     public function render(){
       $url = 'https://expopyme.com.ar/mailing/expopyme_2018/';
       $table = '<table>';
       $str = '';
-      $views = [
-
-      'aval_federal',
-      'bgs-seguros',
-      'buspack',
-      'cocom',
-      'colppy',
-      'fibercorp',
-      'gema',
-      'hr',
-      'iadef',
-      'inemo',
-      'institucional',
-      'meemba',
-      'nocrm',
-      'promote',
-      'rpa',
-      'salto',
-      'vistage',
-
-      ];
-
+      $views = [];
+    $directory = "C:/Users/Sergio/Desktop/DATOLA";
+    $images = glob("$directory/*.{jpg,png,bmp}", GLOB_BRACE);
+    foreach($images as $image)$views[] = explode('.',basename($image))[0];
+    //dd($views);
 
       foreach ($views as $key => $value) {
         $html = view('expopyme',["name"=>$value,"url"=>$url])->render();
         $val = $url.$value.'.html';
         $table.='<tr><td>'.$value.'</td>'.'<td><a href="'.$val.'" target="_blank">'.$val.'</a></td></tr>';
 
-         File::put(storage_path('expopyme') . '/'.$value.'.html', $html);
+         File::put($directory . '/'.$value.'.html', $html);
          $str.=$html;
       }
       $table.='</table>';
